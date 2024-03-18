dbca3f9c56cb11848b5b6fa7c50f879339bde8be
davis <davischilling.dev@gmail.com>
2024-03-18 12:06:53 -0300

    add user update methods unit tests

--- src/users/domain/entities/__tests__/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/user.entity.spec.ts
@@ -13,29 +13,51 @@ describe('UserEntity unit tests', () => {
   it('Constructor method should create a new UserEntity', () => {
     expect(sut).toBeInstanceOf(UserEntity)
     expect(sut.props).toEqual(props)
-    expect(sut.props.createdAt).toBeInstanceOf(Date)
+    expect(sut.createdAt).toBeInstanceOf(Date)
   })
 
   it('username getter should return the username', () => {
-    expect(sut.props.username).toBeDefined()
-    expect(sut.props.username).toBe(props.username)
-    expect(typeof sut.props.username).toBe('string')
+    expect(sut.username).toBeDefined()
+    expect(sut.username).toBe(props.username)
+    expect(typeof sut.username).toBe('string')
+  })
+
+  it('username setter should update the username', () => {
+    const newUsername = 'new-username'
+    const updateUsernameSpy = jest.spyOn(sut, 'updateUsername')
+
+    sut.updateUsername(newUsername)
+
+    expect(updateUsernameSpy).toHaveBeenCalledTimes(1)
+    expect(updateUsernameSpy).toHaveBeenCalledWith(newUsername)
+    expect(sut.username).toBe(newUsername)
   })
 
   it('email getter should return the email', () => {
-    expect(sut.props.email).toBeDefined()
-    expect(sut.props.email).toBe(props.email)
-    expect(typeof sut.props.email).toBe('string')
+    expect(sut.email).toBeDefined()
+    expect(sut.email).toBe(props.email)
+    expect(typeof sut.email).toBe('string')
   })
 
   it('password getter should return the password', () => {
-    expect(sut.props.password).toBeDefined()
-    expect(sut.props.password).toBe(props.password)
-    expect(typeof sut.props.password).toBe('string')
+    expect(sut.password).toBeDefined()
+    expect(sut.password).toBe(props.password)
+    expect(typeof sut.password).toBe('string')
+  })
+
+  it('password setter should update the password', () => {
+    const newPassword = 'new-password'
+    const updatePasswordSpy = jest.spyOn(sut, 'updatePassword')
+
+    sut.updatePassword(newPassword)
+
+    expect(updatePasswordSpy).toHaveBeenCalledTimes(1)
+    expect(updatePasswordSpy).toHaveBeenCalledWith(newPassword)
+    expect(sut.password).toBe(newPassword)
   })
 
   it('createdAt getter should return the createdAt date', () => {
-    expect(sut.props.createdAt).toBeDefined()
-    expect(sut.props.createdAt).toBeInstanceOf(Date)
+    expect(sut.createdAt).toBeDefined()
+    expect(sut.createdAt).toBeInstanceOf(Date)
   })
 })
